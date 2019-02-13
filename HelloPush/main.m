77d1151e982765e407740361390d86dce643d64e
Ananth <agirijak@in.ibm.com>
2019-02-13 14:23:10 +0530

    added files

---
+++ HelloPush/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloPush
+//
+//  Created by Anantha Krishnan K G on 12/02/19.
+//  Copyright © 2019 Ananth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
