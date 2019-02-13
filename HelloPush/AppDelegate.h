b532cd5c2ef9e5aaba9c9f1ff16899e5f6e6e132
Ananth <agirijak@in.ibm.com>
2019-02-13 14:22:54 +0530

    added files

---
+++ HelloPush/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  HelloPush
+//
+//  Created by Anantha Krishnan K G on 12/02/19.
+//  Copyright © 2019 Ananth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HelloPush-Swift.h"
+
+@class BMSPushBinder;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
